b04156c2d5534e4175da96fbaf8defe14982510a
Jonno1809 <jonno1809@hotmail.com>
2017-04-01 12:32:03 +1100

    Add Faculties API Endpoint

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -14,4 +14,5 @@ export class ApiEndpoints {
     public static USER_CREATE = ApiEndpoints.USER + '/create';
     public static UNITS = ApiEndpoints.API + '/units';
     public static COURSES = ApiEndpoints.API + '/courses';
+    public static FACULTIES = ApiEndpoints.API + '/faculties';
 }
