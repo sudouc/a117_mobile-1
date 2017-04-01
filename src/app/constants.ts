a45636b7db8246666766cb6cb6688eba037e347b
ComputerGuruInTraining <bernadettecar77@hotmail.com>
2017-04-01 20:09:28 +1100

    Course Details Page Formatted

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -15,5 +15,5 @@ export class ApiEndpoints {
     public static USER_CREATE = ApiEndpoints.USER + '/create';
     public static UNITS = ApiEndpoints.API + '/units';
     public static UNITS_ALL = ApiEndpoints.UNITS + '/all';
-    public static COURSES = ApiEndpoints.API + '/courses';
+    public static COURSE = ApiEndpoints.API + '/course';
 }
