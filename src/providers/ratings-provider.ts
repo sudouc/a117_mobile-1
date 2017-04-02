6eb8f165f517a3d51c00ebb56ae26c4562e02426
Jonno1809 <jonno1809@hotmail.com>
2017-04-02 14:48:35 +1000

    Create getRatings() method in RatingsProvider.ts

--- src/providers/ratings-provider.ts
+++ src/providers/ratings-provider.ts
@@ -17,4 +17,23 @@ export class RatingsProvider {
     console.log('Hello RatingsProvider Provider');
   }
 
+  public getRatings(){
+      return Observable.create(
+          (observable) => {
+              this.http.get(ApiEndpoints.UNITS + ApiExtensions.RATINGS) // Http GET request
+              .map(response => response.json()) // Convert to a JSON object
+              .subscribe(
+                  (data) => {
+                      console.log(data);
+                      observable.next(data);
+                      observable.complete();
+                  },
+                  (error) => {
+                      console.log(error);
+                      observable.error(error);
+                  })
+          }
+      )
+  }
+
 }
