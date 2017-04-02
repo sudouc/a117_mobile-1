78ad6ed14edb149c82c15be7b0e90f1fb8cdbc1c
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-02 12:51:11 +1000

    Create layout of comments and add dummy data

--- src/components/comments/comments.ts
+++ src/components/comments/comments.ts
@@ -15,7 +15,25 @@ export class CommentsComponent {
     @Input('entity-type') entity_type: string;  // The type of entity (e.g. course, unit) used to select which provider to use
     @Input('entity-id') entity_id: string;    // The ID of the entity (used when getting stuff from the provider)
 
-    comments: any[];
+    comments: any[] = [{
+        title: "A Title",
+        name: "User Name",
+        text: "Comment text",
+        user_dp_url: "http://placehold.it/100x100",
+        likes: 20
+    },
+    {
+        title: "A Title",
+        name: "User Name",
+        text: "Comment text",
+        user_dp_url: "http://placehold.it/100x100"
+    },
+    {
+        title: "A Title",
+        name: "User Name",
+        text: "Comment text",
+        user_dp_url: "http://placehold.it/100x100"
+    }];
 
     constructor() {
         console.log('Hello Comments Component');
